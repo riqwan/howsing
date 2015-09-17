abc94e0d96b1d8933e1b94e68abd99f425f75e62
Riqwan Thamir <rmthamir@gmail.com>
2015-09-17 16:10:33 +0530

    add ember

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,15 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require ember
+//= require ember-data
+//= require active-model-adapter
+//= require_self
+//= require ./howsing
+
+// for more details see: http://emberjs.com/guides/application/
+Howsing = Ember.Application.create();
+
 //= require_tree .
---
+++ app/assets/javascripts/router.js
@@ -0,0 +1,5 @@
+// For more information see: http://emberjs.com/guides/routing/
+
+Howsing.Router.map(function() {
+  // this.resource('posts');
+});
